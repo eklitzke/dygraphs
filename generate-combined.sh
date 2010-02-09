3af8e0c2e957a3c95647050b4d856561b8547d79
Evan Klitzke <evan@eklitzke.org>
2010-02-08 19:34:44 -0800

    respect $TMPDIR in generate-combined.sh

--- generate-combined.sh
+++ generate-combined.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 # Generates a single JS file that's easier to include.
 
+if [ -z "$TMPDIR" ]; then
+  TMPDIR=/tmp
+fi
+
 # Pack the dygraphs JS and rgbcolor
 cat \
 dygraph-canvas.js \
 dygraph.js \
 rgbcolor/rgbcolor.js \
 strftime/strftime-min.js \
-> /tmp/dygraph.js
+> ${TMPDIR}/dygraph.js
 
 java -jar yuicompressor-2.4.2.jar /tmp/dygraph.js \
-> /tmp/dygraph-packed.js
+> ${TMPDIR}/dygraph-packed.js
 
 # TODO(danvk): ensure the dygraphs copyright, etc. gets into the packed js.
 
 cat \
-/tmp/dygraph-packed.js \
+${TMPDIR}/dygraph-packed.js \
 > dygraph-combined.js
